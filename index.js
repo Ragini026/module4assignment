ccb83729a91daf298955414b8d1cbd68319ff2c5
Ragini Sinha <96348249+Ragini026@users.noreply.github.com>
2022-08-20 21:14:25 +0530

    uploaded

---
+++ index.js
@@ -0,0 +1,9 @@
+var names = ['Ragini', 'json', 'Amisha', 'Jerry', 'jiya','Rudra', 'Jimmy', 'mary', 'tom']
+for(var i = 0; i < names.length; i++){
+    if(names[i].charAt(0)==='j'||names[i].charAt(0)==='J'){
+        console.log("goodbye " + names[i])
+    }
+    else{
+        console.log("hello " + names[i])
+    }
+}
